a514a877906a2d3c86ced38ed7dff2b4ef9d90f0
Rara Eva Maharani <rara.eva.ft21@mail.umy.ac.id>
2024-01-09 18:43:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMembers() {
+    document.getElementById('members-container').style.display = 'block';
+    document.querySelector('.container').style.display = 'none';
+}
+
+function showWelcome() {
+    document.getElementById('members-container').style.display = 'none';
+    document.querySelector('.container').style.display = 'block';
+}
